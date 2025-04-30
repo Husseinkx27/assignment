f474b4873653ade779ab1933a83737d1fed97a46
Husseinkx27 <167503283+Husseinkx27@users.noreply.github.com>
2025-04-30 22:23:06 +0300

    Add files via upload

---
+++ C1220501/src/main/java/com/ca226/C1220501/C1220501Application.java
@@ -0,0 +1,13 @@
+package com.ca226.C1220501;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C1220501Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C1220501Application.class, args);
+	}
+
+}
---
+++ C1220501/src/test/java/com/ca226/C1220501/C1220501ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ca226.C1220501;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C1220501ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
